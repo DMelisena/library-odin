2e1046eed7fcad38ac270650c383989653b0f007
DMelisena <demonlordx777@gmail.com>
2024-10-15 13:34:39 +0700

    Add delete function to the libraries
    dfas

--- index.js
+++ index.js
@@ -5,5 +5,13 @@ function Book(name,author){
   this.author = author;
 }
 
+const deleteButtons = document.querySelectorAll(".delete");
+deleteButtons.forEach(function(button) {
+  button.addEventListener("click", function() {
+    const parentDiv = this.parentNode; 
+    parentDiv.remove()
+  });
+});
+
 function addBookToLibrary(){
 }
